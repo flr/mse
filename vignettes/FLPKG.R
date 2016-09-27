4d2eb3ccd579ac36debe090003b14afbf07deb8c
Iago Mosqueira <iago@mosqueira.eu>
2016-09-27 14:07:42 +0200

    Version 0.0.1.9001

--- vignettes/FLPKG.R
+++
@@ -1,9 +0,0 @@
-## ---- pkgs, echo=FALSE, message=FALSE------------------------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>")
-library(FLCore)
-# LOAD other packages, including FLPKG, here
-
-## ---- devtools, echo=TRUE, eval=FALSE------------------------------------
-#  	library(devtools)
-#  	install_github('flr/FLPKG')
-
