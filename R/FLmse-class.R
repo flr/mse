d591e736aa94127a7fc7a735fe78ee73d4faa572
Iago Mosqueira <iago@mosqueira.eu>
2019-12-03 23:09:37 +0100

    Moved plot(FLom, FLmse) to avoid warning

--- R/FLmse-class.R
+++ R/FLmse-class.R
@@ -158,16 +158,22 @@ setMethod("plot", signature(x="FLom", y="FLmse"),
 
     args <- list(...)
 
-    cls <- unlist(lapply(args, is, "FLmse"))
-
-    stocks <- lapply(c(list(x, y), args[cls]), stock)
-    names(stocks) <- lapply(stocks, name)
-
-    stocks[[1]] <- window(stocks[[1]], end=dims(stocks[[2]])$minyear)
+    stocks <- lapply(c(list(x, y), args), stock)
+ 
+    # SORT OUT names
+    if(is.null(names(stocks)))
+      names(stocks) <- rep(character(1), length(stocks))
+    idx <- names(stocks) == character(1)
+    names(stocks)[idx] <- unlist(lapply(stocks, name))[idx]
+    idx <- names(stocks) == character(1)
+    names(stocks)[idx] <- c("OM", paste0("MP", seq(sum(idx))))[idx]
 
     plot(FLStocks(stocks))
   }
-)# }}}
+)
+
+
+# }}}
 
 # summary {{{
 setMethod("summary", signature(object="FLmse"),
