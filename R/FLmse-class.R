56e89045d41bd70ac94cc1baa3d9555e81db38b9
Iago Mosqueira <iago@mosqueira.eu>
2020-06-12 17:08:51 +0200

    Changed argument of generics to names

--- R/FLmse-class.R
+++ R/FLmse-class.R
@@ -124,16 +124,13 @@ setReplaceMethod("oem", signature("FLmse", "FLQuant"), function(object, value){
 # args
 
 #' @rdname FLmse-class
-setMethod("args", "FLmse", function(object) object@args)
+setMethod("args", "FLmse", function(name) name@args)
 
 #' @rdname FLmse-class
 setReplaceMethod("args", signature("FLmse", "list"), function(object, value){
 	object@args <- value
 	object
 })
-
-
-
 
 # }}}
 
