bd2c5b73c7a41d37380056e994e0169169ad4b02
Iago Mosqueira <iago@mosqueira.eu>
2021-10-28 17:28:09 +0200

    Moved generics to separate file

---
+++ R/generics.R
@@ -0,0 +1,12 @@
+# generics.R - DESC
+# /generics.R
+
+# Copyright Iago MOSQUEIRA (WMR), 2021
+# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+
+setGeneric("debug", useAsDefault = base::debug)
+setGeneric("undebug", useAsDefault = base::undebug)
+
