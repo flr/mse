b16b9f75f0071ac022a1596276c8730692afc71f
Iago Mosqueira <iago@mosqueira.eu>
2022-02-22 14:27:32 +0100

    New fb module

--- R/fb.R
+++ R/fb.R
@@ -27,3 +27,49 @@ hyperstability.fb <- function(ctrl, beta=1, maxF=2, alpha=maxF^(1-beta), trackin
   list(ctrl=ctrl, tracking=tracking)
 	
 } # }}}
+
+
+effortlimit.fb <- function(ctrl, max=1, tracking, args) {
+
+# merge {{{
+
+setMethod("merge", signature(x="fwdControl", y="fwdControl"),
+  function(x, y, ...) {
+
+  # PARSE args
+  args <- c(list(x, y), list(...))
+
+  # RBIND @target
+  target <- do.call(rbind, lapply(args, slot, "target"))
+
+  # MERGE @iters
+  its <- lapply(args, slot, "iters")
+
+  # rows per ctrl
+  ros <- unlist(lapply(its, nrow))
+  pos <- c(1, ros[-length(ros)] + 1)
+  nros <- sum(ros)
+  
+  # STOP if different iters
+  nits <- unique(unlist(lapply(its, function(x) dim(x)[3])))
+  if(length(nits) > 1)
+    stop("fwdControl objects must have the same number of iters")
+
+  iters <- array(dim=c(nros, 3, nits), dimnames=list(row=seq(nros),
+      val=c("min", "value", "max"), iter=seq(nits)))
+ 
+  for(i in seq(its))
+    iters[seq(pos[i], length=ros[i]), ,]  <- c(its[[i]])
+
+  return(fwdControl(target=target, iters=iters))
+})
+
+# }}}
+ 
+  ctrl <- merge(ctrl,
+    fwdControl(year=ctrl$year, quant="effort", max=max, fishery=1))
+
+  return(list(ctrl=ctrl, tracking=tracking))
+}
+
+
