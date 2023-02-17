112d44bd3a8d603066b16073badd78bfaab38f12
Iago Mosqueira <iago@mosqueira.eu>
2023-02-17 13:55:32 +0100

    Many more options in tac.is, more ICES-like

--- R/is.R
+++ R/is.R
@@ -10,11 +10,22 @@
 # tac.is {{{
 
 
-#' TAC implementation function
+#' TAC implementation system module
 #'
-#' Performs a short term forecast (STF) to hit the target F in year ay+1.
-#' The resulting catch in year ay+1 is the TAC, i.e. the TAC that will result in Fbar = Ftarget.
-#' The STF uses geometric mean recruitment. Fbar in the intermediate years (i.e. years between last data year and ay+1) are set as the mean of the last nsqy years.
+#' Performs a short term forecast (STF) for the target fishing mortality to
+#' obtain the corresponding catch.
+#'
+#' A `fwdControl` object obtained from the 'hcr' step is applied in the
+#' management year (`ay + mlag`) or years (`seq(ay + mlag, ay + mlag + freq`).
+#' An assumption is made on the mortality in the assessment year (`ay`), which
+#' becomes the intermediate year in this projection. By default this is set
+#' to Fbar = Fstatusquo, that is, the same fishing mortality estimated in the 
+#' last data year (`ay - data_lag`).
+#'
+#' - recruitment
+#'
+
+#' The projection uses geometric mean recruitment. Fbar in the intermediate years (i.e. years between last data year and ay+1) are set as the mean of the last nsqy years.
 #' The control argument is a list of parameters:
 #' nsqy - number of years to average over to get Fbar for STF
 #' delta_tac_min - constraint on the TAC
@@ -25,12 +36,14 @@
 #' @param EFF0 The tracking array
 #' @param EFF Not used by this function but may be used by the other implementation functions
 #' @examples
-#' data(sol274)
 
 # ADD initial TAC or F, initac=catch(stk[, ac(iy)])
 
-tac.is <- function(stk, ctrl, args, dtaclow=NA, dtacupp=NA, recyrs=10,
-  fmin=0, initac=catch(stk[, ac(iy - 1)]), tracking) {
+# all years - 2
+# -25 
+
+tac.is <- function(stk, ctrl, args, dtaclow=NA, dtacupp=NA, recyrs=30,
+  fmin=0, initac=catch(stk[, ac(iy - 1)]), Fdevs=fbar(stk) %=% 1, tracking) {
 
   # EXTRACT args
 
@@ -42,51 +55,56 @@ tac.is <- function(stk, ctrl, args, dtaclow=NA, dtacupp=NA, recyrs=10,
   nsqy <- args$nsqy
 
   # PREPARE stk until ay + mlag, biology as in last nsqy years
-
   fut <- stf(stk, end=ay + mlag, wts.nyears=nsqy)
 
   # SET recruitment
-
-	# Set geomean sr relationship
   gmnrec <- exp(yearMeans(log(rec(stk)[, rev(dimnames(stk)$year)[2:recyrs+1]])))
   srr <- predictModel(model=rec~a, params=FLPar(a=gmnrec))
   
-  # GET TAC dy / ay - 1
-
-  if(ay == iy)
-    prev_tac <- rep(c(initac), length=args$it)
-  else
-    prev_tac <- c(tracking[[1]]["isys", ac(dy)])
+  # ADD F deviances
+  ftar <- ctrl$value * Fdevs[, ac(dy)]
 
   # FORECAST for iyrs and my IF mlag > 0, else only my
-  # DEBUG TAC vs Fsquo in year 1?
-
   if(mlag > 0) {
+ 
+    # SET F for intermediate year
+    fsq <- fbar(stk)[, ac(dy)]
 
+    # TODO: ADD catch option
+
+    # CONSTRUCT fwd control
     fctrl <- fwdControl(
+      # ay as intermediate
       list(year=seq(ay - dlag + 1, length=mlag), quant="fbar",
-        value=rep(c(fbar(stk)[, ac(dy)]), mlag)),
-      list(year=ay + mlag, quant="fbar", value=ctrl$value))
+        value=rep(c(fsq), mlag)),
+      # target
+      list(year=ay + mlag, quant="fbar", value=ftar))
 
   } else {
 
     fctrl <- fwdControl(
-      list(year=ay + mlag, quant="fbar", value=ctrl$value))
+      list(year=ay + mlag, quant="fbar", value=ftar))
   }
 
-  fut <- fwd(fut, sr=srr, control=fctrl)
+  # RUN STF fwd
+  fut <- ffwd(fut, sr=srr, control=fctrl)
 
   # EXTRACT catches
   TAC <- c(catch(fut)[, ac(ay + mlag)])
 
-  # APPLY upper TAC limit
-  TAC <- pmin(TAC, c(prev_tac) * dtacupp, na.rm=TRUE)
+  # ID iters where hcr set met trigger and F > fmin
+  id <- c(tracking[[1]]["decision.hcr", ac(ay)] > 2) &
+    c(fbar(fut)[, ac(ay + mlag)] > fmin)
 
-  # ID iters where F > fmin
-  i <- c(fbar(fut)[, ac(ay + mlag)] > fmin)
+  # GET TAC dy / ay - 1
+  if(ay == iy)
+    prev_tac <- rep(c(initac), length=args$it)
+  else
+    prev_tac <- c(tracking[[1]]["isys", ac(ay)])
 
-  # APPLY lower TAC limit, only if F > fmin
-  TAC[i] <- pmax(TAC[i], c(prev_tac)[i] * dtaclow, na.rm=TRUE)
+  # APPLY upper and lower TAC limit, only for id
+  TAC[id] <- pmin(TAC[id], c(prev_tac)[id] * dtacupp)
+  TAC[id] <- pmax(TAC[id], c(prev_tac)[id] * dtaclow)
 
   # CONSTRUCT fwdControl
   ctrl <- fwdControl(list(year=ay + mlag, quant="catch", value=TAC))
@@ -169,7 +187,7 @@ seasonal.is <- function(stk, ctrl, args, ratio=rep(1 / args$ns, args$ns),
 
 # }}}
 
-# allocate.is
+# TODO: TURN splitTAC into allocate.is
 
 # splitTAC.is {{{
 
