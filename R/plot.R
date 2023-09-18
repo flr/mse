3f935ad05d46cac72cd3cd7c65759719fe41ba11
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2023-09-18 12:25:47 +0200

    Added plots

--- R/plot.R
+++ R/plot.R
@@ -6,7 +6,7 @@
 #
 # Distributed under the terms of the EUPL-1.2
 
-# FLo, ... {{{
+# FLo {{{
 setMethod("plot", signature(x="FLo", y="missing"),
   function(x, ...) {
 
@@ -22,6 +22,9 @@ setMethod("plot", signature(x="FLo", y="missing"),
     plot(mets) + ylim(c(0, NA))
   }
 )
+# }}}
+
+# FLo, FLmse {{{
 
 setMethod("plot", signature(x="FLo", y="FLmse"),
   function(x, y, metrics=NULL, ..., window=TRUE) {
@@ -32,6 +35,31 @@ setMethod("plot", signature(x="FLo", y="FLmse"),
     plot(x, y, metrics=metrics, window=window)
   }
 )
+# }}}
+
+# FLo, fwdControl {{{
+
+setMethod("plot", signature(x="FLo", y="fwdControl"),
+  function(x, y, fill="#E69F00", ...) {
+
+    yrs <- range(y$year)
+
+    # CREATE standard plot
+    p <- plot(x, ...)
+
+    # GET x variable
+    if(rlang::as_name(p$mapping$x) == "date") {
+      yrs <- range(p$data[with(p$data, year %in% yrs), "date"])
+    }
+
+    p + geom_vline(xintercept=yrs[1], alpha=0.4) +
+      geom_vline(xintercept=yrs[2], alpha=0.2) +
+      annotate("rect", xmin = yrs[1], xmax = yrs[2], ymin = -Inf, ymax = Inf,
+        fill = fill, alpha=0.1)
+  }
+) # }}}
+
+# FLom, list {{{
 
 setMethod("plot", signature(x="FLom", y="list"),
   function(x, y, window=TRUE, ...) {
@@ -59,37 +87,6 @@ setMethod("plot", signature(x="FLom", y="list"),
     do.call(plotListFLQuants, c(list(x=mets), args))
   }
 )
-# }}}
-
-# FLmse, missing {{{
-setMethod("plot", signature(x="FLmse", y="missing"),
-  function(x, ...) {
-    # PLOT om
-    plot(om(x), ...)
-  }
-)
-
-# }}}
-
-# .plotom {{{
-.plotom <- function(stocks, window=TRUE, ...) {
- 
-  # WINDOW om
-  if(isTRUE(window))
-    maxyear <- min(unlist(lapply(stocks[-1], function(i) dims(i)$minyear)))
-  else
-    maxyear <- min(unlist(lapply(stocks[-1], function(i) dims(i)$maxyear)))
-    
-  stocks[[1]] <- window(stocks[[1]], end=maxyear)
-
-  # SORT OUT names
-  if(is.null(names(stocks)))
-    names(stocks) <- c("OM", paste0("MP", seq(length(stocks) - 1)))
- 
-  # PLOT FLStocks
-  plot(FLStocks(stocks), ...)
-
-}
 # }}}
 
 # FLombf {{{
@@ -149,6 +146,18 @@ setMethod("plot", signature(x="FLombf", y="missing"),
     }
  }) # }}}
 
+# FLombf, FLmse, ... {{{
+setMethod("plot", signature(x="FLombf", y="FLmse"),
+  function(x, y, window=TRUE, ...) {
+
+    y <- c(list(y), list(...))
+
+    plot(x, y, window=window)
+
+  }
+)
+# }}}
+
 # FLombf, list, ... {{{
 setMethod("plot", signature(x="FLombf", y="list"),
   function(x, y, window=TRUE) {
@@ -171,24 +180,34 @@ setMethod("plot", signature(x="FLombf", y="list"),
   })
 # }}}
 
-# FLo, fwdControl {{{
-
-setMethod("plot", signature(x="FLo", y="fwdControl"),
-  function(x, y, fill="#E69F00", ...) {
-
-    yrs <- range(y$year)
-
-    # CREATE standard plot
-    p <- plot(x, ...)
-
-    # GET x variable
-    if(rlang::as_name(p$mapping$x) == "date") {
-      yrs <- range(p$data[with(p$data, year %in% yrs), "date"])
-    }
-
-    p + geom_vline(xintercept=yrs[1], alpha=0.4) +
-      geom_vline(xintercept=yrs[2], alpha=0.2) +
-      annotate("rect", xmin = yrs[1], xmax = yrs[2], ymin = -Inf, ymax = Inf,
-        fill = fill, alpha=0.1)
+# FLmse, missing {{{
+setMethod("plot", signature(x="FLmse", y="missing"),
+  function(x, ...) {
+    # PLOT om
+    plot(om(x), ...)
   }
-) # }}}
+)
+
+# }}}
+
+# .plotom {{{
+.plotom <- function(stocks, window=TRUE, ...) {
+ 
+  # WINDOW om
+  if(isTRUE(window))
+    maxyear <- min(unlist(lapply(stocks[-1], function(i) dims(i)$minyear)))
+  else
+    maxyear <- min(unlist(lapply(stocks[-1], function(i) dims(i)$maxyear)))
+    
+  stocks[[1]] <- window(stocks[[1]], end=maxyear)
+
+  # SORT OUT names
+  if(is.null(names(stocks)))
+    names(stocks) <- c("OM", paste0("MP", seq(length(stocks) - 1)))
+ 
+  # PLOT FLStocks
+  plot(FLStocks(stocks), ...)
+
+}
+# }}}
+
