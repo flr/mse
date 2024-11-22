0a7f094b43ed2cef8120e9ac60ad6eefdebbb51b
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2024-11-22 23:19:06 +0100

    Added refpts needed to compute all statistics

--- data-raw/sol274.R
+++ data-raw/sol274.R
@@ -107,7 +107,7 @@ oem <- FLoem(observations=obs, deviances=devs, method=sampling.oem)
 
 # SAVE
 
-save(om, oem, file="../data/sol274.RData", compress="xz")
+save(om, oem, file="../data/sol274.rda", compress="xz")
 
 
 # --- TEST
