00890635fb2db039c62a5d69cca30eb150d53be6
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2026-01-04 18:36:18 +0100

    v2.2.3.9813

---
+++ tests/data.R
@@ -0,0 +1,25 @@
+# data.R - DESC
+# /home/mosqu003/Projects/FLR/code/mse/mse/tests/data.R
+
+# Copyright (c) WMR, 2026.
+# Author: Iago MOSQUEIRA <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+
+# FLom
+
+data(plesim)
+
+# FLom seasonal
+
+# FLom 2 sex 
+
+# FLombf (1, 1)
+
+# FLombf (1, N)
+
+# FLombf (2, 1)
+
+# FLombf (2, N)
+
